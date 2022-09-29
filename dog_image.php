bda669697a5b41fb1d68b25fe5486605b0672050
Kobayashi-Yukari <0609mucc@gmail.com>
2022-09-29 15:15:01 +0900

    愛犬の画像送信機能完了

---
+++ dog_image.php
@@ -0,0 +1,39 @@
+<?php
+
+$array = array(
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_1672.JPG',
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_1736.JPG',
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_1795.JPG',
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_1798.JPG',
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_1850.JPG',
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_1900JPG',
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_1963.JPG',
+'https://pompon-blog.com/me_bot/images/dog_images/IMG_2752.JPG',
+);
+
+$rand_keys = array_rand($array, 1);
+
+
+      error_log(print_r($array[$rand_keys], true) . "\n", 3, 'debug.log');
+
+$rand_keys = array_rand($array, 1);
+
+$client->replyMessage(array(
+'replyToken' => $event['replyToken'],
+'messages' => array(
+    array(
+	'type' => 'text',
+	'text' => '我が家の愛犬「ぱん」ちゃんです',
+    ),
+    array(
+    'type' => 'image',
+	'originalContentUrl' => $array[$rand_keys],
+	'previewImageUrl' => $array[$rand_keys],
+    ),
+    array(
+	'type' => 'text',
+	'text' => 'タップする度に画像が変わるよ',
+    ),
+)
+));
+?>
